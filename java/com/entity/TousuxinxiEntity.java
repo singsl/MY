1bffbcb248ded102320c5328bc6fa30ba1f9e0f2
singsl <95468712+singsl@users.noreply.github.com>
2023-04-22 00:26:48 +0800

    Add files via upload

---
+++ java/com/entity/TousuxinxiEntity.java
@@ -0,0 +1,235 @@
+package com.entity;
+
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.lang.reflect.InvocationTargetException;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.apache.commons.beanutils.BeanUtils;
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.enums.FieldFill;
+import com.baomidou.mybatisplus.enums.IdType;
+
+
+/**
+ * 投诉信息
+ * 数据库通用操作实体类（普通增删改查）
+ */
+@TableName("tousuxinxi")
+public class TousuxinxiEntity<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+
+	public TousuxinxiEntity() {
+		
+	}
+	
+	public TousuxinxiEntity(T t) {
+		try {
+			BeanUtils.copyProperties(this, t);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 主键id
+	 */
+	@TableId
+	private Long id;
+	/**
+	 * 标题
+	 */
+					
+	private String biaoti;
+	
+	/**
+	 * 投诉用户
+	 */
+					
+	private String tousuyonghu;
+	
+	/**
+	 * 头数内容
+	 */
+					
+	private String toushuneirong;
+	
+	/**
+	 * 投诉日期
+	 */
+				
+	@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd")
+	@DateTimeFormat 		
+	private Date tousuriqi;
+	
+	/**
+	 * 用户名
+	 */
+					
+	private String yonghuming;
+	
+	/**
+	 * 姓名
+	 */
+					
+	private String xingming;
+	
+	/**
+	 * 是否审核
+	 */
+					
+	private String sfsh;
+	
+	/**
+	 * 审核回复
+	 */
+					
+	private String shhf;
+	
+	/**
+	 * 用户id
+	 */
+					
+	private Long userid;
+	
+	
+	@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat
+	private Date addtime;
+
+	public Date getAddtime() {
+		return addtime;
+	}
+	public void setAddtime(Date addtime) {
+		this.addtime = addtime;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * 设置：标题
+	 */
+	public void setBiaoti(String biaoti) {
+		this.biaoti = biaoti;
+	}
+	/**
+	 * 获取：标题
+	 */
+	public String getBiaoti() {
+		return biaoti;
+	}
+	/**
+	 * 设置：投诉用户
+	 */
+	public void setTousuyonghu(String tousuyonghu) {
+		this.tousuyonghu = tousuyonghu;
+	}
+	/**
+	 * 获取：投诉用户
+	 */
+	public String getTousuyonghu() {
+		return tousuyonghu;
+	}
+	/**
+	 * 设置：头数内容
+	 */
+	public void setToushuneirong(String toushuneirong) {
+		this.toushuneirong = toushuneirong;
+	}
+	/**
+	 * 获取：头数内容
+	 */
+	public String getToushuneirong() {
+		return toushuneirong;
+	}
+	/**
+	 * 设置：投诉日期
+	 */
+	public void setTousuriqi(Date tousuriqi) {
+		this.tousuriqi = tousuriqi;
+	}
+	/**
+	 * 获取：投诉日期
+	 */
+	public Date getTousuriqi() {
+		return tousuriqi;
+	}
+	/**
+	 * 设置：用户名
+	 */
+	public void setYonghuming(String yonghuming) {
+		this.yonghuming = yonghuming;
+	}
+	/**
+	 * 获取：用户名
+	 */
+	public String getYonghuming() {
+		return yonghuming;
+	}
+	/**
+	 * 设置：姓名
+	 */
+	public void setXingming(String xingming) {
+		this.xingming = xingming;
+	}
+	/**
+	 * 获取：姓名
+	 */
+	public String getXingming() {
+		return xingming;
+	}
+	/**
+	 * 设置：是否审核
+	 */
+	public void setSfsh(String sfsh) {
+		this.sfsh = sfsh;
+	}
+	/**
+	 * 获取：是否审核
+	 */
+	public String getSfsh() {
+		return sfsh;
+	}
+	/**
+	 * 设置：审核回复
+	 */
+	public void setShhf(String shhf) {
+		this.shhf = shhf;
+	}
+	/**
+	 * 获取：审核回复
+	 */
+	public String getShhf() {
+		return shhf;
+	}
+	/**
+	 * 设置：用户id
+	 */
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
+	/**
+	 * 获取：用户id
+	 */
+	public Long getUserid() {
+		return userid;
+	}
+
+}
---
+++ java/com/entity/model/TousuxinxiModel.java
@@ -0,0 +1,200 @@
+package com.entity.model;
+
+import com.entity.TousuxinxiEntity;
+
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.io.Serializable;
+ 
+
+/**
+ * 投诉信息
+ * 接收传参的实体类  
+ *（实际开发中配合移动端接口开发手动去掉些没用的字段， 后端一般用entity就够用了） 
+ * 取自ModelAndView 的model名称
+ */
+public class TousuxinxiModel  implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	 			
+	/**
+	 * 投诉用户
+	 */
+	
+	private String tousuyonghu;
+		
+	/**
+	 * 头数内容
+	 */
+	
+	private String toushuneirong;
+		
+	/**
+	 * 投诉日期
+	 */
+		
+	@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat 
+	private Date tousuriqi;
+		
+	/**
+	 * 用户名
+	 */
+	
+	private String yonghuming;
+		
+	/**
+	 * 姓名
+	 */
+	
+	private String xingming;
+		
+	/**
+	 * 是否审核
+	 */
+	
+	private String sfsh;
+		
+	/**
+	 * 审核回复
+	 */
+	
+	private String shhf;
+		
+	/**
+	 * 用户id
+	 */
+	
+	private Long userid;
+				
+	
+	/**
+	 * 设置：投诉用户
+	 */
+	 
+	public void setTousuyonghu(String tousuyonghu) {
+		this.tousuyonghu = tousuyonghu;
+	}
+	
+	/**
+	 * 获取：投诉用户
+	 */
+	public String getTousuyonghu() {
+		return tousuyonghu;
+	}
+				
+	
+	/**
+	 * 设置：头数内容
+	 */
+	 
+	public void setToushuneirong(String toushuneirong) {
+		this.toushuneirong = toushuneirong;
+	}
+	
+	/**
+	 * 获取：头数内容
+	 */
+	public String getToushuneirong() {
+		return toushuneirong;
+	}
+				
+	
+	/**
+	 * 设置：投诉日期
+	 */
+	 
+	public void setTousuriqi(Date tousuriqi) {
+		this.tousuriqi = tousuriqi;
+	}
+	
+	/**
+	 * 获取：投诉日期
+	 */
+	public Date getTousuriqi() {
+		return tousuriqi;
+	}
+				
+	
+	/**
+	 * 设置：用户名
+	 */
+	 
+	public void setYonghuming(String yonghuming) {
+		this.yonghuming = yonghuming;
+	}
+	
+	/**
+	 * 获取：用户名
+	 */
+	public String getYonghuming() {
+		return yonghuming;
+	}
+				
+	
+	/**
+	 * 设置：姓名
+	 */
+	 
+	public void setXingming(String xingming) {
+		this.xingming = xingming;
+	}
+	
+	/**
+	 * 获取：姓名
+	 */
+	public String getXingming() {
+		return xingming;
+	}
+				
+	
+	/**
+	 * 设置：是否审核
+	 */
+	 
+	public void setSfsh(String sfsh) {
+		this.sfsh = sfsh;
+	}
+	
+	/**
+	 * 获取：是否审核
+	 */
+	public String getSfsh() {
+		return sfsh;
+	}
+				
+	
+	/**
+	 * 设置：审核回复
+	 */
+	 
+	public void setShhf(String shhf) {
+		this.shhf = shhf;
+	}
+	
+	/**
+	 * 获取：审核回复
+	 */
+	public String getShhf() {
+		return shhf;
+	}
+				
+	
+	/**
+	 * 设置：用户id
+	 */
+	 
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
+	
+	/**
+	 * 获取：用户id
+	 */
+	public Long getUserid() {
+		return userid;
+	}
+			
+}
